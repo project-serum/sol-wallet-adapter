ad1bf5868c118bd7cfd55b2e262eb299833b0cbe
Gary Wang <gary@garywang.net>
2020-08-06 23:12:17 -0700

    Updates

---
+++ precache-manifest.1d7044acce9624079940f2cb5cf3eda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68fbcf949b5be7171d1ba60fa864351",
+    "url": "/sol-wallet-adapter/index.html"
+  },
+  {
+    "revision": "d65181be98a5e2fed5fe",
+    "url": "/sol-wallet-adapter/static/css/main.66ec011b.chunk.css"
+  },
+  {
+    "revision": "37329a38642633b9afec",
+    "url": "/sol-wallet-adapter/static/js/2.4f4b3921.chunk.js"
+  },
+  {
+    "revision": "316598ae44b554ab20dacb68d3fe8228",
+    "url": "/sol-wallet-adapter/static/js/2.4f4b3921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65181be98a5e2fed5fe",
+    "url": "/sol-wallet-adapter/static/js/main.d8453055.chunk.js"
+  },
+  {
+    "revision": "36c6f5ebb8a53b56c13c",
+    "url": "/sol-wallet-adapter/static/js/runtime-main.a64c95aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3a2e597f813918487cca8fadcffa34cc.js"
+  "/sol-wallet-adapter/precache-manifest.1d7044acce9624079940f2cb5cf3eda5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sol-wallet-adapter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
