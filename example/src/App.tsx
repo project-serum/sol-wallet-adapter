c21b266076c79c477d9855019034d9f439419699
Ian Macalinao <github@igm.pub>
2021-06-29 15:57:16 -0500

    Use single package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
-import Wallet from '@project-serum/sol-wallet-adapter';
+import Wallet from '../../';
 import {
   Connection,
   SystemProgram,
