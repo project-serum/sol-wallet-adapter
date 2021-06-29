f5451e4436ad46f05a2d96644e4bb66bd48f947f
Ian Macalinao <github@igm.pub>
2021-06-29 11:20:54 -0500

    fix example build

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,42 +1,50 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Wallet from '@project-serum/sol-wallet-adapter';
-import { Connection, SystemProgram, Transaction, clusterApiUrl } from '@solana/web3.js';
+import {
+  Connection,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+} from '@solana/web3.js';
 
-function toHex(buffer) {
+function toHex(buffer: Buffer) {
   return Array.prototype.map
     .call(buffer, (x) => ('00' + x.toString(16)).slice(-2))
     .join('');
 }
 
-function App() {
-  const [logs, setLogs] = useState([]);
-  function addLog(log) {
+function App(): React.ReactElement {
+  const [logs, setLogs] = useState<string[]>([]);
+  function addLog(log: string) {
     setLogs((logs) => [...logs, log]);
   }
 
   const network = clusterApiUrl('devnet');
   const [providerUrl, setProviderUrl] = useState('https://www.sollet.io');
   const connection = useMemo(() => new Connection(network), [network]);
-  const urlWallet = useMemo(() => new Wallet(providerUrl, network), [
-    providerUrl,
-    network,
-  ]);
+  const urlWallet = useMemo(
+    () => new Wallet(providerUrl, network),
+    [providerUrl, network],
+  );
   const injectedWallet = useMemo(() => {
     try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       return new Wallet((window as any).solana, network);
     } catch (e) {
       console.log(`Could not create injected wallet: ${e}`);
       return null;
     }
   }, [network]);
-  const [selectedWallet, setSelectedWallet] = useState(undefined);
+  const [selectedWallet, setSelectedWallet] = useState<
+    Wallet | undefined | null
+  >(undefined);
   const [, setConnected] = useState(false);
   useEffect(() => {
     if (selectedWallet) {
       selectedWallet.on('connect', () => {
         setConnected(true);
-        addLog('Connected to wallet ' + selectedWallet.publicKey.toBase58());
+        addLog('Connected to wallet ' + selectedWallet.publicKey?.toBase58());
       });
       selectedWallet.on('disconnect', () => {
         setConnected(false);
@@ -51,19 +59,23 @@ function App() {
 
   async function sendTransaction() {
     try {
+      const pubkey = selectedWallet?.publicKey;
+      if (!pubkey || !selectedWallet) {
+        throw new Error('wallet not connected');
+      }
       const transaction = new Transaction().add(
         SystemProgram.transfer({
-          fromPubkey: selectedWallet.publicKey,
-          toPubkey: selectedWallet.publicKey,
+          fromPubkey: pubkey,
+          toPubkey: pubkey,
           lamports: 100,
-        })
+        }),
       );
       addLog('Getting recent blockhash');
       transaction.recentBlockhash = (
         await connection.getRecentBlockhash()
       ).blockhash;
       addLog('Sending signature request to wallet');
-      transaction.feePayer = selectedWallet.publicKey;
+      transaction.feePayer = pubkey;
       const signed = await selectedWallet.signTransaction(transaction);
       addLog('Got signature, submitting transaction');
       const signature = await connection.sendRawTransaction(signed.serialize());
@@ -78,7 +90,11 @@ function App() {
 
   async function signMessage() {
     try {
-      const message = "Please sign this message for proof of address ownership.";
+      if (!selectedWallet) {
+        throw new Error('wallet not connected');
+      }
+      const message =
+        'Please sign this message for proof of address ownership.';
       addLog('Sending message signature request to wallet');
       const data = new TextEncoder().encode(message);
       const signed = await selectedWallet.sign(data, 'hex');
@@ -103,15 +119,21 @@ function App() {
       </div>
       {selectedWallet && selectedWallet.connected ? (
         <div>
-          <div>Wallet address: {selectedWallet.publicKey.toBase58()}.</div>
+          <div>Wallet address: {selectedWallet.publicKey?.toBase58()}.</div>
           <button onClick={sendTransaction}>Send Transaction</button>
           <button onClick={signMessage}>Sign Message</button>
-          <button onClick={() => selectedWallet.disconnect()}>Disconnect</button>
+          <button onClick={() => selectedWallet.disconnect()}>
+            Disconnect
+          </button>
         </div>
       ) : (
         <div>
-          <button onClick={() => setSelectedWallet(urlWallet)}>Connect to Wallet</button>
-          <button onClick={() => setSelectedWallet(injectedWallet)}>Connect to Injected Wallet</button>
+          <button onClick={() => setSelectedWallet(urlWallet)}>
+            Connect to Wallet
+          </button>
+          <button onClick={() => setSelectedWallet(injectedWallet)}>
+            Connect to Injected Wallet
+          </button>
         </div>
       )}
       <hr />
